851ff15902a9c2fb6eed608dfd15f79ce43e14cb
lars-de-vries <112874252+lars-de-vries@users.noreply.github.com>
2022-09-22 14:05:03 +0200

    voortgangsgesprek 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,21 @@
-// JavaScript Document
-console.log("hi");
+
+var hamburgerMenu = document.querySelector("header button");
+
+hamburgerMenu.addEventListener("click", toggleMenu);
+
+function toggleMenu() {  
+  var deNav = document.querySelector("nav");
+  deNav.classList.toggle("toonMenu");
+
+  var hamburgerIcon = document.querySelector("header button img");
+  if (deNav.classList.contains("toonMenu")){
+    hamburgerIcon.src="assets/hamburger-menu-close.svg";
+  } else{
+    hamburgerIcon.src="assets/hamburger-menu.svg";
+  }
+};
+
+
+
+
+
